72a2853057462f6320aeef4c1b5af4ffed03c835
xabarjim442 <xabarjim442@ieshnosmachado.org>
2024-11-06 10:41:45 +0100

    Creacion del archivo

--- ejercicio Diccionario/ejercicio_diccionario.py
+++ ejercicio Diccionario/ejercicio_diccionario.py
@@ -19,3 +19,16 @@ def guardar_lista(diccionario, nombreFichero):
         for cancion, autor in diccionario.items():
             #fichero.write(cancion+"-"+autor+"\n")
             fichero.write(f"{cancion} - {autor}\n")
+
+def agregar_cancion(diccionario, cancion, autor):
+    if cancion in diccionario:
+        print(f"La cancion {cancion} ya se encuentra en la lista.")
+    else:
+        diccionario[cancion]= autor
+        print(f"La cancion {cancion} se ha añadido correctamente.")
+
+def eliminar_cancion(diccionario, cancion):
+    if cancion in diccionario:
+        del diccionario[cancion]
+    else:
+        print(f"La cancion {cancion} no se encuentra en la lista.")
