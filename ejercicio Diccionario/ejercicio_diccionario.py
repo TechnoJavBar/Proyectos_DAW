bf9959d1b82c08ffb2863dbeb48163971b56bd76
xabarjim442 <xabarjim442@ieshnosmachado.org>
2024-11-06 12:43:32 +0100

    Creacion de la función eliminar() Creación de la función buscar_canción()

--- ejercicio Diccionario/ejercicio_diccionario.py
+++ ejercicio Diccionario/ejercicio_diccionario.py
@@ -6,29 +6,24 @@ def cargar_lista(nombreFichero):
     with open(nombreFichero,"r") as fichero:
         for linea in fichero:
             cancion, autor, genero = linea.strip().split("-")
+            
+            #se debe de crear un diccionario por cada cancion
             diccionario= {
                 "cancion": cancion, 
                 "autor": autor, 
                 "genero": genero
-                } #se debe de crear un diccionario por cada cancion
+                } 
             lista_diccionario.append(diccionario)
             
     
     return lista_diccionario
 
 def agregar_cancion(lista, cancion, autor, genero):
-    esta= False #Se crea un booleano para poder comprobar si está la canción ya introducida
-    # if cancion in diccionario:
-    #     print(f"La cancion {cancion} ya se encuentra en la lista.")
-    # else:
-    #     diccionario[cancion]= autor
-    #     print(f"La cancion {cancion} se ha añadido correctamente.")
     
-    for diccionario in lista:
-        if cancion == diccionario["cancion"]:
-            print(f"La cancion {cancion} ya se encuentra en la lista.")
-            esta= True
-    if not esta:
+    esta = buscar_cancion(cancion,lista)
+    if esta:
+        print(f"La cancion {cancion} ya se encuentra registrada")
+    else:
         diccionario= {
             "cancion": cancion,
             "autor": autor,
@@ -38,26 +33,39 @@ def agregar_cancion(lista, cancion, autor, genero):
         print(f"La cancion: {cancion} de {autor} se ha añadido correctamente.")
     
 
-def eliminar_cancion(lista, cancion):
-    # if cancion in diccionario:
-    #     del diccionario[cancion]
-    # else:
-    #     print(f"La cancion {cancion} no se encuentra en la lista.")
+def eliminar_cancion(lista, cancion, artista):
 
+    found= buscar_cancion(cancion,lista)
+
+    if not found:
+        print(f"La cancion {cancion} del artista {artista} no se encuentra en la lista.")
+    else:
+        for diccionario in lista:
+            if diccionario['cancion']==cancion and diccionario['autor']==artista:
+                lista.remove(diccionario)
+                print(f"La cancion {cancion} de {artista} ha sido eliminada.")
     return lista
+
+def buscar_cancion(nombre, lista):
+    esta = False
+    for diccionario in lista:
+        if diccionario['cancion'] == nombre:
+            esta= True
+    
+    return esta
+    print(esta)
 
 def guardar_lista(lista, nombreFichero):
     with open(nombreFichero,"w") as fichero:
         for diccionario in lista:
-            fichero.write(f"{diccionario['cancion']} - {diccionario['autor']} - {diccionario['genero']}\n")
+            fichero.write(f"{diccionario['cancion']}-{diccionario['autor']}-{diccionario['genero']}\n")
         # for cancion, autor in diccionario.items():
         #     fichero.write(cancion+"-"+autor+"\n")
         #     fichero.write(f"{cancion} - {autor}\n")
 
 ############################################################################
 playlist= cargar_lista("ejercicio Diccionario\\playlist.txt")
-print(playlist)
 
-print("##############################################################################")
-print(agregar_cancion(playlist,"Nuestra cancion","Alvaro de luna","pop"))
+print(agregar_cancion(playlist,"Brisa","Paul thin","pop"))
+eliminar_cancion(playlist,"Nuestra cancion","Alvaro de luna")
 guardar_lista(playlist, "ejercicio Diccionario\\playlist.txt")
